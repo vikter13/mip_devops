e08d15af2baedcadf794fce7e5235055b946417f
Nikita Ziby <nik.yashin.03@inbox.ru>
2025-04-19 12:33:02 +0300

    Adding analysators Test errors 1

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-@login_manager.user_loader
+@login_manager.user_loader # password: admin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
